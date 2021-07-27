920b6c4a1ce44d509e008d6b8f3c58ae01467c4a
Ethan Henderson <ethan.henderson.1998@gmail.com>
2021-07-28 00:27:03 +0100

    Initial workflow

---
+++ noxfile.py
@@ -0,0 +1,24 @@
+import nox
+
+
+def parse_requirements(path):
+    with open(path, mode="r", encoding="utf-8") as f:
+        deps = (d.strip() for d in f.readlines())
+        return [d for d in deps if not d.startswith(("#", "-r"))]
+
+
+@nox.session(python=["3.6", "3.7", "3.8", "3.9", "3.10"], reuse_venv=True)
+def tests(session: nox.Session) -> None:
+    session.run("pytest", "-s", "--verbose", "--log-level=INFO")
+
+
+@nox.session(reuse_venv=True)
+def check_formatting(session: nox.Session) -> None:
+    black_version = next(
+        filter(
+            lambda d: d.startswith("black"),
+            parse_requirements("./requirements-dev.txt"),
+        )
+    ).split("==")[1]
+    session.install(f"black=={black_version}")
+    session.run("black", ".", "--check")
