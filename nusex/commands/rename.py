ca2e5f47a277cbedc32f3c8a9a4eb542e64df32d
Ethan Henderson <ethan.henderson.1998@gmail.com>
2021-07-30 18:22:50 +0100

    Add argument checks to rename command

--- nusex/commands/rename.py
+++ nusex/commands/rename.py
@@ -1,9 +1,22 @@
 import os
+import re
 
 from nusex import CONFIG_DIR
 
+from ..errors import NoMatchingTemplates, TemplateBuildError
+
+NAME_REGEX = re.compile("[^a-z0-9_]+")
+
 
 def run(old_name, new_name):
+    if not os.path.isfile(CONFIG_DIR / f"{old_name}.nsx"):
+        raise NoMatchingTemplates(f"no template named '{old_name}' exists")
+    if NAME_REGEX.search(new_name):
+        raise TemplateBuildError(
+            "template names can only contain lower case letters, numbers, "
+            "and underscores"
+        )
+
     os.rename(CONFIG_DIR / f"{old_name}.nsx", CONFIG_DIR / f"{new_name}.nsx")
     print(
         f"🎉 Template successfully renamed from '{old_name}' to '{new_name}'!"
