148711fda923ffff373b31bc8137025490da4cdd
Ethan Henderson <ethan.henderson.1998@gmail.com>
2021-08-30 02:13:07 +0100

    Modify boilerplate

--- nusex/__init__.py
+++ nusex/__init__.py
@@ -1,21 +1,50 @@
+# Copyright (c) 2021, Ethan Henderson
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its
+#    contributors may be used to endorse or promote products derived from
+#    this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 __productname__ = "nusex"
-__version__ = "0.4.0"
+__version__ = "1.0.0.dev0"
 __description__ = "A project templating utility for Python."
 __url__ = "https://github.com/parafoxia/nusex"
 __docs__ = "https://nusex.readthedocs.io/en/latest/"
 __author__ = "Ethan Henderson"
 __author_email__ = "ethan.henderson.1998@gmail.com"
-__license__ = "BSD 3-Clause"
+__license__ = "BSD-3-Clause"
 __bugtracker__ = "https://github.com/parafoxia/nusex/issues"
 
 import os
 from pathlib import Path
 
+# Prevent conflicts during development.
+SUFFIX = f"nusex{'-dev' if 'dev' in __version__ else ''}"
+
 if os.name == "nt":
-    CONFIG_DIR = Path.home() / ".nusex"
+    CONFIG_DIR = Path.home() / f".{SUFFIX}"
     TEMP_DIR = CONFIG_DIR / "tmp"
 else:
-    CONFIG_DIR = Path.home() / ".config/nusex"
-    TEMP_DIR = Path("/tmp/nusex")
-
-from . import commands
+    CONFIG_DIR = Path.home() / f".config/{SUFFIX}"
+    TEMP_DIR = Path(f"/tmp/{SUFFIX}")
