83ccfc64f691bbde85c1da8b83aa7c2de29c709f
Ethan Henderson <ethan.henderson.1998@gmail.com>
2021-08-30 17:55:42 +0100

    Delete old, redundant tests

--- tests/test_999_cleanup.py
+++
@@ -1,9 +0,0 @@
-import os
-import shutil
-
-
-def test_cleanup():
-    # Cleans up after the tests are complete.
-    os.chdir("..")
-    shutil.rmtree("./awesome_pkg")
-    assert not os.path.isdir("./awesome_pkg")
