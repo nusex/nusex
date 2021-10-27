62a8acf08c0a547377085a7c297aa90d1bc1642f
Ethan Henderson <ethan.henderson.1998@gmail.com>
2021-10-27 01:12:44 +0100

    Ignore .nusexmeta files by default

--- nusex/cli/commands/build.py
+++ nusex/cli/commands/build.py
@@ -228,12 +228,13 @@ def setup(subparsers):
             "a comma-separated list of directories to ignore; look at the "
             "docs for more information about advanced ignoring syntaxes "
             "(default: .direnv,.eggs,.git,.hg,.mypy_cache,.nox,.tox,.venv,"
-            "venv,.svn,_build,build,dist,buck-out,.pytest-cache,*.egg-info)"
+            "venv,.svn,_build,build,dist,buck-out,.pytest-cache,.nusexmeta,"
+            "*.egg-info)"
         ),
         metavar="DIRS",
         default=(
             ".direnv,.eggs,.git,.hg,.mypy_cache,.nox,.tox,.venv,venv,.svn,"
-            "_build,build,dist,buck-out,.pytest-cache,*.egg-info"
+            "_build,build,dist,buck-out,.pytest-cache,.nusexmeta,*.egg-info"
         ),
         type=_options_as_set,
     )
