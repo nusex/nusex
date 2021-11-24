a549f5a4ab98a6959faedb822d83498a787c5ee7
Ethan Henderson <ethan.henderson.1998@gmail.com>
2021-11-24 07:58:00 +0000

    Simplify test suite

--- noxfile.py
+++ noxfile.py
@@ -26,64 +26,156 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-import runpy
-from importlib import import_module
+from __future__ import annotations
+
+import os
+import re
 from pathlib import Path
 
 import nox
-from nox import options
 
-from pipelines.config import D
+PROJECT_DIR = Path(__file__).parent
+TEST_DIR = PROJECT_DIR / "tests"
 
-options.sessions = []
+PROJECT_NAME = Path(__file__).parent.stem
 
-for p in (Path(__file__).parent / "pipelines").glob("*.py"):
-    if p.stem == "config":
-        continue
+CHECK_PATHS = (
+    str(PROJECT_DIR / PROJECT_NAME),
+    str(TEST_DIR),
+    str(PROJECT_DIR / "noxfile.py"),
+    str(PROJECT_DIR / "setup.py"),
+)
 
-    mod = import_module(f"pipelines.{p.stem}")
-    for name in dir(mod):
-        if name.startswith("_"):
-            continue
+DEP_PATTERN = re.compile("([a-zA-Z0-9-_]*)[=~<>,.0-9ab]*")
 
-        attr = getattr(mod, name)
-        if isinstance(attr, nox._decorators.Func):
-            options.sessions.append(name)
+if os.name == "nt":
+    TEST_CONFIG_DIR = Path.home() / ".nusex-test"
+else:
+    TEST_CONFIG_DIR = Path.home() / ".config/nusex-test"
 
-    runpy.run_path(f"{p}")
-
-
-# The following sessions are only ever run manually, as they are
-# generally either perform actions (which automatic checks shouldn't
-# do), or just display something to the console that isn't useful during
-# CI sessions.
+ZIP_PATH = Path(__file__).parent.parent / "tests/data/nusex-test.zip"
 
 
-@nox.session(reuse_venv=True)
-def show_coverage(session: nox.Session) -> None:
-    session.install("-U", D["coverage"])
+def fetch_installs(*categories: str) -> list[str]:
+    installs = []
 
-    if not (Path(__file__).parent / ".coverage").is_file():
-        session.skip("No coverage to check")
+    with open(PROJECT_DIR / "requirements-dev.txt") as f:
+        in_cat = None
 
-    session.run("coverage", "report", "-m")
+        for line in f:
+            if line.startswith("#") and line[2:].strip() in categories:
+                in_cat = True
+                continue
 
+            if in_cat:
+                if line == "\n":
+                    in_cat = False
+                    continue
 
-@nox.session(reuse_venv=True)
-def format(session: nox.Session) -> None:
-    session.install("-U", D["black"], D["isort"])
-    session.run("isort", ".")
-    session.run("black", ".")
+                installs.append(line.strip())
 
-
-@nox.session(reuse_venv=True)
-def build_docs(session: nox.Session) -> None:
-    session.install("-U", D["sphinx"], D["furo"], ".")
-    session.cd("./docs")
-    session.run("make", "html")
+    return installs
 
 
 @nox.session(reuse_venv=True)
-def build_package(session: nox.Session) -> None:
-    session.install("build==0.7.0")
-    session.run("python", "-m", "build")
+def tests(session: nox.Session) -> None:
+    # with zipfile.ZipFile(ZIP_PATH) as z:
+    #     z.extractall(TEST_CONFIG_DIR)
+
+    session.install("-U", *fetch_installs("Tests"), ".")
+    # session.run(
+    #     "coverage",
+    #     "run",
+    #     "--omit",
+    #     "tests/*",
+    #     "-m",
+    #     "pytest",
+    #     "--testdox",
+    #     "--log-level=INFO",
+    # )
+
+    # if TEST_CONFIG_DIR.is_dir():
+    #     try:
+    #         shutil.rmtree(TEST_CONFIG_DIR)
+    #     except PermissionError:
+    #         # Some weird permissions error with Windows we don't
+    #         # care about.
+    #         ...
+
+
+@nox.session(reuse_venv=True)
+def check_formatting(session: nox.Session) -> None:
+    session.install("-U", *fetch_installs("Formatting"))
+    session.run("black", ".", "--check")
+
+
+@nox.session(reuse_venv=True)
+def check_imports(session: nox.Session) -> None:
+    session.install("-U", *fetch_installs("Imports"))
+    # flake8 doesn't use the gitignore so we have to be explicit.
+    session.run(
+        "flake8",
+        *CHECK_PATHS,
+        "--select",
+        "F4",
+        "--extend-ignore",
+        "E,F,W",
+        "--extend-exclude",
+        "__init__.py",
+    )
+    session.run("isort", ".", "-cq")
+
+
+@nox.session(reuse_venv=True)
+def check_typing(session: nox.Session) -> None:
+    session.install("-U", *fetch_installs("Typing"), "-r", "requirements.txt")
+    session.run("mypy", *CHECK_PATHS)
+
+
+@nox.session(reuse_venv=True)
+def check_line_lengths(session: nox.Session) -> None:
+    session.install("-U", *fetch_installs("Line lengths"))
+    session.run("len8", *CHECK_PATHS, "-lx", "data")
+
+
+@nox.session(reuse_venv=True)
+def check_licensing(session: nox.Session) -> None:
+    missing = []
+
+    for p in [
+        *(PROJECT_DIR / PROJECT_NAME).rglob("*.py"),
+        *TEST_DIR.glob("*.py"),
+        *PROJECT_DIR.glob("*.py"),
+    ]:
+        with open(p) as f:
+            if not f.read().startswith("# Copyright (c)"):
+                missing.append(p)
+
+    if missing:
+        session.error(
+            f"\n{len(missing):,} file(s) are missing their licenses:\n"
+            + "\n".join(f" - {file}" for file in missing)
+        )
+
+
+@nox.session(reuse_venv=True)
+def check_spelling(session: nox.Session) -> None:
+    session.install("-U", *fetch_installs("Spelling"))
+    session.run("codespell", *CHECK_PATHS)
+
+
+@nox.session(reuse_venv=True)
+def check_safety(session: nox.Session) -> None:
+    paths = list(PROJECT_DIR.glob("requirements*.txt"))
+    if not Path("nusex.egg-info").is_dir():
+        # The git pull locally confuses safety somewhat.
+        paths.append(PROJECT_DIR / "docs/requirements.txt")
+
+    installs = []
+    for p in paths:
+        installs.extend(["-r", f"{p}"])
+
+    # Needed due to https://github.com/pypa/pip/pull/9827.
+    session.install("-U", "pip")
+    session.install("-U", *installs)
+    session.run("safety", "check", "--full-report")
